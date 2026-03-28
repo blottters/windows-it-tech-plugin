cb77560bf112ea3a4ca0668830f71f262fd6b7f9
gavin <gavinisbell99@gmail.com>
2026-03-28 05:59:40 -0400

    Remove build artifact

--- do_git.bat
+++
@@ -1,5 +0,0 @@
-cd /d C:\Users\gavin\windows-it-tech-plugin
-git add -A
-git status
-git commit -m "v2.0.0: Add allowed-tools, argument-hint, hooks, fix marketplace.json location"
-git push origin master
